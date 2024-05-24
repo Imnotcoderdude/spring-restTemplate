add4c7e047ca95484c48a3afc8ae716f8a81d4ab
Imnotcoderdude <topsolo7@naver.com>
2024-05-24 13:52:09 +0900

    init commit

---
+++ src/main/java/com/sparta/jpaadvence/JpaAdvenceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAdvenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/jpaadvence/JpaAdvenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
